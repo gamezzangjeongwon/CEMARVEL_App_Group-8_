b3f1f3d896d51a34d6d8956272832882cad9bfb6
gamezzangjeongwon <sjw020729@yonsei.ac.kr>
2023-05-09 15:40:14 +0900

    Initial Commit

---
+++ CEM_App_Group#8/CEM_App_Group_8App.swift
@@ -0,0 +1,17 @@
+//
+//  CEM_App_Group_8App.swift
+//  CEM_App_Group#8
+//
+//  Created by 서정원 on 2023/05/09.
+//
+
+import SwiftUI
+
+@main
+struct CEM_App_Group_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CEM_App_Group#8/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CEM_App_Group#8
+//
+//  Created by 서정원 on 2023/05/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
